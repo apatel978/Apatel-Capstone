b1fc7eec928455d7878d1c8fe50e284a10bc640e
Akshar <aksharpatel978@gmail.com>
2024-08-14 19:26:41 -0400

    github down so log deleted

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -14,7 +14,6 @@ function SignupFormModal() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
-  console.log('hi');
 
   const handleSubmit = (e) => {
     e.preventDefault();

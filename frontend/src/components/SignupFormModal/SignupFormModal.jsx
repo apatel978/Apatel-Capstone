22e5c97a85c709d455ac75b8ba4884586122455f
Akshar <aksharpatel978@gmail.com>
2024-08-14 19:24:31 -0400

    added a log for build

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -14,6 +14,7 @@ function SignupFormModal() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
+  console.log('hi')
 
   const handleSubmit = (e) => {
     e.preventDefault();
